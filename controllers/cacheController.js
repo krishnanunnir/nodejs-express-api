87c1ec026b31f17ff720809eb9405f9ff4546aa8
Krishnanunni R <krishnanunnir97@gmail.com>
2022-05-15 13:13:57 +0530

    feature: added handling for create or update and get all cache value endpoints

--- controllers/cacheController.js
+++ controllers/cacheController.js
@@ -1,14 +1,24 @@
-let cache = {};
+const express = require('express');
+const Cache = require('../models/cache');
 
 exports.getCacheValueByKey = (req, res) => {
-  cach;
+  res.send('Not implmeneted yet');
 };
 exports.getAllCacheValues = (req, res) => {
-  res.send('Not implmeneted yet');
+  Cache.find({}).exec((err, cacheList) => {
+    if (err) res.send(500, { error: err });
+    res.send(cacheList);
+  });
 };
 
-exports.createOrUpdateCache = (req, res) => {
-  res.send('Not implmeneted yet');
+exports.createOrUpdateCache = (req, res, next) => {
+  const query = { key: req.body.key };
+  const cacheRecord = req.body;
+
+  Cache.findOneAndUpdate(query, cacheRecord, { upsert: true }, (err, data) => {
+    if (err) return res.send(500, { error: err });
+    return res.status(200).send(`Succesfully saved.`);
+  });
 };
 
 exports.removeAllKeysFromCache = (req, res) => {
