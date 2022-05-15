a15fa933d51608988e08c151e4a5e8583e46d7e7
Krishnanunni R <krishnanunnir97@gmail.com>
2022-05-15 12:22:11 +0530

    fix: setting standard spaces in eslint and fixing for existing files

--- models/cache.js
+++ models/cache.js
@@ -1,19 +1,19 @@
 const { Schema, model } = require('mongoose');
 
 const CacheSchema = new Schema({
-    key: { type: String, rquired: true },
-    value: { type: String, required: true },
-    last_refresh_at: { type: Date, required: true },
-    ttl: { type: Number, required: true }, // ttl stored in milliseconds
+  key: { type: String, rquired: true },
+  value: { type: String, required: true },
+  last_refresh_at: { type: Date, required: true },
+  ttl: { type: Number, required: true }, // ttl stored in milliseconds
 });
 
 CacheSchema.virtual('isExpired').get(() => {
-    const now = new Date();
-    const diffTime = Math.abs(now - this.last_refresh_at);
-    if (diffTime > this.ttl) {
-        return true;
-    }
-    return false;
+  const now = new Date();
+  const diffTime = Math.abs(now - this.last_refresh_at);
+  if (diffTime > this.ttl) {
+    return true;
+  }
+  return false;
 });
 
 module.exports = model('Cache', CacheSchema);
--- routes/cache.js
+++ routes/cache.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const {
   getAllCacheValues,
+  getCacheValueByKey,
   createOrUpdateCache,
-  removeAllKeysFromCache,
   removeAllKeysFromCache,
   removeFromCacheByKey,
 } = require('../controllers/cacheController');
+
 const router = express.Router();
 
-router.get(`cache/`, getAllCacheValues);
-router.get(`cache/:key`, getCacheValueByKey);
-router.put(`cache/`, createOrUpdateCache);
-router.delete(`cache/`, removeAllKeysFromCache);
-router.delete(`cache/:key`, removeFromCacheByKey);
+router.get('cache/', getAllCacheValues);
+router.get('cache/:key', getCacheValueByKey);
+router.put('cache/', createOrUpdateCache);
+router.delete('cache/', removeAllKeysFromCache);
+router.delete('cache/:key', removeFromCacheByKey);
